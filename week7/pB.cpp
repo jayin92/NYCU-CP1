7fc5d4bb65913a3f265e8d5e6b44756cf7d970c3
jayin92 <jayin920805@gmail.com>
2022-04-07 11:03:36 +0800

    add week7

---
+++ week7/pB.cpp
@@ -0,0 +1,137 @@
+// :80 <enter>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<double,double> pdd;
+#define SQ(i) ((i)*(i))
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define SZ(i) int(i.size())
+#define FOR(i, j, k, in) for (int i=j ; i<(k) ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=(k) ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define X first
+#define Y second
+#ifdef jayinnn
+#define TIME(i) Timer i(#i)
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<", ";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+class Timer {
+private:
+    string scope_name;
+    chrono::high_resolution_clock::time_point start_time;
+public:
+    Timer (string name) : scope_name(name) {
+        start_time = chrono::high_resolution_clock::now();
+    }
+    ~Timer () {
+        auto stop_time = chrono::high_resolution_clock::now();
+        auto length = chrono::duration_cast<chrono::microseconds>(stop_time - start_time).count();
+        double mlength = double(length) * 0.001;
+        debug(scope_name, mlength);
+    }
+};
+#else
+#define TIME(i)
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll MOD = 1000000007;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int iNF = 0x3f3f3f3f;
+const ll MAXN = 100005;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+vector<vector<int>> check(10);
+
+bool ok(string s){
+    int sz = s.size();
+    for(int i=1;i<sz;i++){
+        bool flag = true;
+        for(auto j: check[s[i-1]-'0']){
+            if(j == s[i] - '0'){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    for(int i=0;;i++){
+        if(n - i >= 0){
+            if(ok(to_string(n-i))){
+                cout << n-i << endl;
+                return;
+            }
+        }
+        if(ok(to_string(n+i))){
+            cout << n+i << endl;
+            return;
+        }
+        
+    }
+}
+
+/********** Good Luck :) **********/
+int main () {
+    TIME(main);
+    IOS();
+    check[0] = {0};
+    check[1] = {0,1,2,3,4,5,6,7,8,9};
+    check[2] = {0,2,3,5,6,8,9};
+    check[3] = {3,6,9};
+    check[4] = {0,4,5,6,7,8,9};
+    check[5] = {0,5,6,8,9};
+    check[6] = {6,9};
+    check[7] = {0,7,8,9};
+    check[8] = {0,8,9};
+    check[9] = {9};
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
